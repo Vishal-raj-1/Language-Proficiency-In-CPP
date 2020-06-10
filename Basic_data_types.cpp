6f3554c51ddd4575a42c579f1cd84d3fb86edaaa
Vishal <59874304+Vishal-raj-1@users.noreply.github.com>
2020-06-10 23:15:25 +0530

    Basic_data_types.cpp added

--- Basic_data_types.cpp
+++ Basic_data_types.cpp
@@ -4,11 +4,8 @@ using namespace std;
 
 int main()
 {
-    int a;
-    long b;
-    char c;
-    float f;
-    double d;
+    int a; long b; char c; float f; double d;
+    
     cin >> a >> b >> c >> f >> d;
 
     cout << a << "\n" << b << "\n" << c << "\n";
